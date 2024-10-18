35bedb86b71c17aea93ee21dea761a409278f466
sistemas19avanceytec <147069465+sistemas19avanceytec@users.noreply.github.com>
2024-10-18 11:37:19 -0600

    Revert "Merge branch 'main' of https://github.com/Sistemas22ATP/Fine-Tuning-OpenAI"
    This reverts commit 226a6c0af59795a357fcf13dd3e62224dd7d1f96, reversing
    changes made to caaa8a7cbde87032eb73991318bf269439337136.

--- src/index.js
+++ src/index.js
@@ -14,11 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", apiRoute);
 
-app.use(express.static('/public'));
-
-app.get('/', (req, res) => {
-    res.sendFile("./index.html");
-}); 
-
-const PORT = process.env.PORT || 3000; 
-app.listen(PORT, () => {console.log("EL PUERTO ES: " + PORT)});
+app.get("/", (req, res)=>{
+    res.json({prompt: "response"});
+})
+app.listen(PORT, () => {console.log("EL PUERTO ES: " + PORT)}) 
